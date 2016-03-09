1fa2cb6436214f2b06313da3ad795719ed0dbe8c
Lndqw12 <COMP222012016@mail.ru>
2016-03-09 17:53:22 +0300

    kodUD
    kodUD

--- ud.php
+++ ud.php
@@ -1,1 +1,19 @@
+﻿<meta charset ="utf-8">
+<?php
+$host='localhost';
+$dbname='3pk'; //имя базы данных
+$user='redactor'; //пользователь
+$password=''; //пароль от пользователя
+require_once 'connection.php'; //имя глвного пчп 
+if(isset($_POST['id']))
+{
+    $link = mysqli_connect($host, $user, $password, $database) 
+      or die("Ошибка " . mysqli_error($link)); 
+    $id = mysqli_real_escape_string($link, $_POST['id']);
 
+    $query ="DROP FROM "_" WHERE id = '$id'";
+
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+    mysqli_close($link);
+}
+?>
